28879ff3af364bcba1f8d1933971882a337eb5a4
powerticket <jwp0530@gmail.com>
2020-11-12 23:48:34 +0900

    vue youtube video api

--- vue/vue-basic/src/App.vue
+++ vue/vue-basic/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-    </div>
-    <router-view/>
+    
   </div>
 </template>
 
