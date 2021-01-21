9a941522ffc3093b895e10c41129e80959a44ead
powerticket <jwp0530@gmail.com>
2021-01-21 23:35:52 +0900

    Add 210121 study

---
+++ chapter5/test.js
@@ -0,0 +1,5 @@
+const t1 = 'test'
+let t2
+t2 = void t1
+console.log(t1)
+console.log(t2)
---
+++ function.js
@@ -0,0 +1,13 @@
+const f1 = function () {
+  console.log('this is function1')
+}
+function f2 () {
+  console.log('this is function2')
+}
+const f3 = () => {
+  console.log('this is function3')
+}
+
+f1()
+f2()
+f3()
